48546bcba33aa4c9c541535c1b3dad27630f3202
Nikhil Nagaraj <nikhil_nagaraj@epam.com>
2024-08-20 14:33:08 +0530

    updated xpath

--- src/test/java/com/epam/training/nikhil_nagaraj/optional_task1/Task1.java
+++ src/test/java/com/epam/training/nikhil_nagaraj/optional_task1/Task1.java
@@ -21,13 +21,13 @@ public class Task1 {
             textArea.click();
             textArea.sendKeys("Hello from WebDriver");
 
-            WebElement selectDropDown = driver.findElement(By.xpath("//*[@id=\"w0\"]/div[5]/div[1]/div[4]/div/span/span[1]/span/span[2]"));
+            WebElement selectDropDown = driver.findElement(By.id("select2-postform-expiration-container"));
             selectDropDown.click();
         WebElement pasteExpiration = driver.findElement(By.xpath("//li[text()='10 Minutes']"));
             pasteExpiration.click();
         WebElement pasteTitle = driver.findElement(By.id("postform-name"));
             pasteTitle.sendKeys("helloweb");
-        driver.findElement(By.xpath("//*[@id=\"w0\"]/div[5]/div[1]/div[10]/button")).click();
-
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.close();
     }
 }
